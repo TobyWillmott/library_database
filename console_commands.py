3ae8935a359c08900a25dfaf3730c8c0a1c49dbf
tobywillmott <tobwil0910@highgateschool.org.uk>
2023-02-24 08:54:20 +0000

    initial commit

---
+++ console_commands.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from models import Book, Author, Publisher
+
+authors = [Author(author_name="Roald Dahl"),
+           Author(author_name="J.K. Rowling"),
+           Author(author_name="George Orwell"),
+           Author(author_name="David Walliams"),
+           ]
+publishers = [Publisher(publisher_name="Puffin"),
+              Publisher(publisher_name="Macmillan"),
+              Publisher(publisher_name="Bloomsbury")
+              ]
+books = [Book(title="The boy in the dress", ISBN_number="9780007329267", num_pages=365, publication_date=2000, publisher_id=1),
+         Book(title="1984", ISBN_number="9780033329467", num_pages=265, publication_date=1948, publisher_id=2),
+         Book(title="Harry Potter and the philosphers stone", ISBN_number="9782207329211", num_pages=165, publication_date=1995, publisher_id=3),
+         Book(title="James and the giant peach", ISBN_number="243908732467", num_pages=144, publication_date=1934, publisher_id=1),
+         ]
+
+authors[0].books.append(books[3])
+authors[1].books.append(books[2])
+authors[2].books.append(books[1])
+authors[3].books.append(books[0])
+
+engine = create_engine("sqlite:///library.sqlite", echo=True)
+
+with Session(engine) as sess:
+    sess.add_all(publishers)
+    sess.commit()
+
---
+++ models.py
@@ -0,0 +1,44 @@
+from sqlalchemy import Column, Integer, String, Table, UniqueConstraint, ForeignKey
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
+
+book_author = Table("book_author",
+                    Base.metadata,
+                    Column("id", Integer, primary_key=True),
+                    Column("book_id", ForeignKey("book.book_id")),
+                    Column("author_id", ForeignKey("author.author_id")),
+                    UniqueConstraint("book_id", "author_id")
+                    )
+
+
+class Book(Base):
+    __tablename__ = "book"
+    book_id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, nullable=False)
+    ISBN_number = Column(Integer, nullable=False)
+    num_pages = Column(Integer, nullable=False)
+    publication_date = Column(Integer, nullable=False)
+    publisher_id = Column(Integer, ForeignKey("publisher.publisher_id"))
+    authors = relationship("Author",
+                                  secondary=book_author,
+                                  order_by="(Author.author_name)",
+                                  back_populates="books"
+                                  )
+
+
+class Author(Base):
+    __tablename__ = "author"
+    author_id = Column(Integer, primary_key=True, autoincrement=True)
+    author_name = Column(String, nullable=False)
+    books = relationship("Book",
+                                secondary=book_author,
+                                order_by="(Book.title)",
+                                back_populates="authors"
+                                )
+
+
+class Publisher(Base):
+    __tablename__ = "publisher"
+    publisher_id = Column(Integer, primary_key=True, autoincrement=True)
+    publisher_name = Column(String, nullable=False)
